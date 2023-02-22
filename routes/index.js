9d7c6ef892a4f72b9f1ddd43580001a487a12145
Adrián Ramón <adrianramon2003@gmail.com>
2023-02-22 11:07:05 +0100

    Estilos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pool = require('../db')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.get('/fotos', async (req, res, next)=>{
+  const [resul] = await pool.query('SELECT * FROM fotos')
+  console.log(resul);
+  res.render('fotos', {resul})
+  // res.render('fotos')
+})
+
+
+router.get('/addfotos', (req, res, next)=>{
+  res.render('addFotos')
+})
+
+router.post('/addfotos', async (req, res, next)=>{
+  console.log(req.body)
+  await pool.query('INSERT INTO fotos set ?',req.body)
+
+  res.redirect('/fotos')
+})
 
 module.exports = router;
